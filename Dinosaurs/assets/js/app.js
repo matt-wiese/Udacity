fc70737ab47f02bfd42bdbe71699a2a67d0e29f7
mwiese <matt.wiese206@gmail.com>
2020-12-12 08:26:27 -0800

    updated functions.js

--- Dinosaurs/assets/js/app.js
+++ Dinosaurs/assets/js/app.js
@@ -1,111 +1,117 @@
+window.onload = function () {
+  // Variables
+  const url =
+    "https://raw.githubusercontent.com/paulbgomez/JavaScript-OOP/main/assets/json/dino.json";
+  let dataJSON; // Dino[];
+  let dataForm = {};
+  const form = document.getElementById("dino-compare");
+  const grid = document.getElementById("grid");
+  const newBtn = document.getElementById("nuevo-btn");
+  newBtn.style.display = "none";
 
-   window.onload = function(){
+  // charging info from the JSON file // OK
+  dataJSON = readTextFile(url);
 
-    // Variables
-    const url = 'https://raw.githubusercontent.com/paulbgomez/JavaScript-OOP/main/assets/json/dino.json';
-    let dataJSON; // Dino[];
-    let dataForm = {};
-    const form = document.getElementById('dino-compare');
-    const grid = document.getElementById('grid');
-    const newBtn = document.getElementById('nuevo-btn');
-    newBtn.style.display = 'none';
+  // Button to start comparing
+  const button = document.getElementById("btn");
+  button.addEventListener("click", initApp);
 
-
-    // charging info from the JSON file // OK
+  function initApp() {
+    // 1º Cherge dataJSON // OK
     dataJSON = readTextFile(url);
+    let objectJSON = JSON.parse(dataJSON);
+    let listaDino = objectJSON.Dinos;
 
-    // Button to start comparing
-    const button = document.getElementById('btn');
-    button.addEventListener('click', initApp);
+    // 2º dataForm charges user input // OK
+    getUserData(dataForm);
+    let person = new Human(dataForm);
 
-    function initApp() {
+    // 3º hide form // OK
+    removeForm();
 
-      // 1º Cherge dataJSON // OK
-      dataJSON = readTextFile(url);
-      let objectJSON = JSON.parse(dataJSON);
-      let listaDino = objectJSON.Dinos;
+    // 4º Shuffle dino aray with human always in the middle // OK
+    let fullArray = valueMiddleArray(shuffleArray(listaDino), human);
 
-      // 2º dataForm charges user input // OK
-      getUserData(dataForm);
-      let person = new Human(dataForm);
-
-      // 3º hide form // OK
-      removeForm();
-
-      // 4º Shuffle dino aray with human always in the middle // OK
-      let fullArray = valueMiddleArray(shuffleArray(listaDino), person);
-
-      // 5ª Generate a random array that determines which animals will be compared Ej -> [1, 0, 0, 0, 0, 1, 1, 0, 0] // OK
-      let arrayImplicationsAnimals = [];
-      function fillArrayImplications() {
-        for(let i = 0; i < fullArray.length; i++){
-          let implica = Math.floor(Math.random()*2);
-          arrayImplicationsAnimals.push(implica);
-        }
-        arrayImplicationsAnimals[4] = 0;
+    // 5ª Generate a random array that determines which animals will be compared Ej -> [1, 0, 0, 0, 0, 1, 1, 0, 0] // OK
+    let arrayImplicationsAnimals = [];
+    function fillArrayImplications() {
+      for (let i = 0; i < fullArray.length; i++) {
+        let implica = Math.floor(Math.random() * 2);
+        arrayImplicationsAnimals.push(implica);
       }
-      fillArrayImplications();
+      arrayImplicationsAnimals[4] = 0;
+    }
+    fillArrayImplications();
 
-      let tilesHTML = '';
-  
-      // 6º Loop that goes through fullArray and compare with dinos or displays the fact // OK
-      for(let i = 0; i < fullArray.length; i++){
+    let tilesHTML = "";
 
-        tilesHTML += '<div class="grid-item">';
-        tilesHTML += '<h3>' + fullArray[i].species + '</h3>';
+    // 6º Loop that goes through fullArray and compare with dinos or displays the fact // OK
+    for (let i = 0; i < fullArray.length; i++) {
+      tilesHTML += '<div class="grid-item">';
+      tilesHTML += "<h3>" + fullArray[i].species + "</h3>";
 
-        if(arrayImplicationsAnimals[i]){
-          let comparisonMethod = Math.floor(Math.random() * 3);
-          switch(comparisonMethod){
-            case 0:
-              tilesHTML += '<img src="assets/images/' + fullArray[i].species.toLowerCase() + '.png" />';
-              tilesHTML += '<h4>' + person.methodWeight(fullArray[i]) + '</h4>';
-              break;
-            case 1:
-              tilesHTML += '<img src="assets/images/' + fullArray[i].species.toLowerCase() + '.png" />';
-              tilesHTML += '<h4>' + person.methodDiet(fullArray[i]) + '</h4>';
-                break;
-            case 2:
-              tilesHTML += '<img src="assets/images/' + fullArray[i].species.toLowerCase() + '.png" />';
-              tilesHTML += '<h4>' + person.methodHeight(fullArray[i]) + '</h4>';
-              break;
-          }
+      if (arrayImplicationsAnimals[i]) {
+        let comparisonMethod = Math.floor(Math.random() * 3);
+        switch (comparisonMethod) {
+          case 0:
+            tilesHTML +=
+              '<img src="assets/images/' +
+              fullArray[i].species.toLowerCase() +
+              '.png" />';
+            tilesHTML += "<h4>" + person.methodWeight(fullArray[i]) + "</h4>";
+            break;
+          case 1:
+            tilesHTML +=
+              '<img src="assets/images/' +
+              fullArray[i].species.toLowerCase() +
+              '.png" />';
+            tilesHTML += "<h4>" + person.methodDiet(fullArray[i]) + "</h4>";
+            break;
+          case 2:
+            tilesHTML +=
+              '<img src="assets/images/' +
+              fullArray[i].species.toLowerCase() +
+              '.png" />';
+            tilesHTML += "<h4>" + person.methodHeight(fullArray[i]) + "</h4>";
+            break;
         }
-        else if(i === 4){
-          tilesHTML += '<img src="assets/images/human.png" />';
-          tilesHTML += '<h4></h4>';
-        }
-        else{
-          tilesHTML += '<img src="assets/images/' + fullArray[i].species.toLowerCase() + '.png" />';
-          tilesHTML += '<h4>' + fullArray[i].fact + '</h4>';
-        }
-        tilesHTML += '</div>';
+      } else if (i === 4) {
+        tilesHTML += '<img src="assets/images/human.png" />';
+        tilesHTML += "<h4></h4>";
+      } else {
+        tilesHTML +=
+          '<img src="assets/images/' +
+          fullArray[i].species.toLowerCase() +
+          '.png" />';
+        tilesHTML += "<h4>" + fullArray[i].fact + "</h4>";
       }
-
-      // Paint the tiles // OK
-      grid.innerHTML = tilesHTML;
-
-      showNewButton();
+      tilesHTML += "</div>";
     }
 
-    function getUserData(input) {
-      input.species = document.getElementById('name').value;
-      input.diet    = document.getElementById('diet').value;
-      input.weight  = parseInt(document.getElementById('weight').value);
-      input.height  = parseInt(document.getElementById('height').value);
-      }
+    // Paint the tiles // OK
+    grid.innerHTML = tilesHTML;
 
-    const removeForm = () => {
-      form.style.display = 'none';
-    };
+    showNewButton();
+  }
 
-    function showNewButton() {
-      newBtn.innerHTML = 'Compare Me Again';
-      newBtn.style.display = 'block';
-      newBtn.addEventListener('click', refreshBoard);
-    }
+  function getUserData(input) {
+    input.species = document.getElementById("name").value;
+    input.diet = document.getElementById("diet").value;
+    input.weight = parseInt(document.getElementById("weight").value);
+    input.height = parseInt(document.getElementById("height").value);
+  }
 
-    function refreshBoard() {
-      initApp();
-    }
-   };
+  const removeForm = () => {
+    form.style.display = "none";
+  };
+
+  function showNewButton() {
+    newBtn.innerHTML = "Compare Me Again";
+    newBtn.style.display = "block";
+    newBtn.addEventListener("click", refreshBoard);
+  }
+
+  function refreshBoard() {
+    initApp();
+  }
+};
---
+++ Dinosaurs/assets/js/functions.js
@@ -0,0 +1,36 @@
+// Function that reads an external file
+// https://stackoverflow.com/questions/39989756/how-do-i-make-a-function-that-returns-the-value-of-a-local-text-file-in-javascri
+
+function readTextFile(file) {
+  let rawFile = new XMLHttpRequest();
+  let allText = "";
+  rawFile.open("GET", file, false);
+  rawFile.onreadystatechange = function () {
+    if (rawFile.readyState === 4) {
+      if (rawFile.status === 200 || rawFile.status == 0) {
+        allText = rawFile.responseText;
+      }
+    }
+  };
+
+  rawFile.send(null);
+
+  return allText;
+}
+
+// Shuffles a given array to produce random re-ordering when invoked
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array?noredirect=1&lq=1
+
+function shuffleArray(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
+
+// Places object in middle of array
+function valueMiddleArray(arr, human) {
+  arr.splice(arr.length / 2, 0, human);
+  return arr;
+}
