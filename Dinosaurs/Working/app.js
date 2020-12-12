3ff07a09afbae5bad6230293a6d246fddef9fb3a
mwiese <matt.wiese206@gmail.com>
2020-12-12 08:26:27 -0800

    commented and ready for first submission

--- Dinosaurs/Working/app.js
+++ Dinosaurs/Working/app.js
@@ -1,46 +1,4 @@
-// Create Dino Objects
-
-// Create Human Object
-
-// Use IIFE to get human data from form
-
-// Generate Tiles for each Dino in Array
-
-// Add tiles to DOM
-
-// Remove form from screen
-
-// On button click, prepare and display infographic
-
-// $.getJSON("https://matt-wiese.github.io/Dinosaurs/assets/json/dino.json", function(json) {
-//     console.log(json);
-// let objectJSON = JSON.parse(dataJSON);
-// let listaDino = objectJSON.Dinos;
-// });
-
-// global variables
-
-function readDinosJSON() {
-  let rawFile = new XMLHttpRequest();
-  let allText = "";
-  rawFile.open(
-    "GET",
-    "https://matt-wiese.github.io/Dinosaurs/assets/json/dino.json",
-    false
-  );
-  rawFile.onreadystatechange = function () {
-    if (rawFile.readyState === 4) {
-      if (rawFile.status === 200 || rawFile.status == 0) {
-        allText = rawFile.responseText;
-      }
-    }
-  };
-  rawFile.send(null);
-  let objectJSON = JSON.parse(allText);
-  dinos = objectJSON.Dinos;
-  return dinos;
-}
-
+// constructor function to create objects
 function DinoCon(dinoObj, units) {
   this.species = dinoObj.species;
   this.diet = dinoObj.diet;
@@ -112,38 +70,29 @@ const protoDino = {
 // created with the DinoCon constructor will inherit these prototypes
 DinoCon.prototype = protoDino;
 
-/**
- * @description Creates the dinosaur object array by calling constructor, inserts a human placeholder for proper iteration later
- * @param {string} units 'metric' or 'imperial' for height and weight
- * @returns {Array} Array of dinosaur objects from constructor
- */
+// creates array of DinoCon objects for an array which is then shuffled
 function createDinoArray(units) {
   readDinosJSON();
   const dinoArray = [];
 
   dinos.forEach(function (dino) {
     dinoArray.push(new DinoCon(dino, units));
+    shuffleArray(dinoArray);
   });
 
   // Insert the human placeholder here so that iteration works properly
   // in the grid element construction.  Human should be in the centre square.
-  // MAY NEED TO REVERT 'createHumanElement()' back to "human placeholder"
-  dinoArray.splice(4, 0, "placeholder");
+  dinoArray.splice(4, 0, "human");
 
   return dinoArray;
 }
 
-/**
- * @description Creates a grid element for a dinosaur object
- * @param {Object} dinoData An object representing a single dinosaur
- * @param {Object} humanData Data grabbed from the user's input form
- * @returns {Element} An element to be added to the grid in the UI
- */
+// takes in dinoObj from DinoCon and humanData to create a grid element
 function createDinoElement(dinoObj, humanData) {
   let fact;
-  // Project requirement is that pigeon should always return the same fact,
-  // so we rig the random number for pigeon
-  // Dinosaurs each return one of 6 facts randomly chosen here
+
+  // checks if pigean so fact will never change
+  // this will return a fact at random
   const randomNumber =
     dinoObj.species === "Pigeon" ? 2 : Math.round(Math.random() * 5);
 
@@ -170,7 +119,7 @@ function createDinoElement(dinoObj, humanData) {
       fact = "Dinosaurs are cool!";
   }
 
-  // Create the new grid item with title, image, and chosen fact
+  // creates div element with object from DinoCon
   const newDiv = document.createElement("div");
   newDiv.className = "grid-item";
   newDiv.innerHTML = `<h3>${
@@ -182,10 +131,8 @@ function createDinoElement(dinoObj, humanData) {
   return newDiv;
 }
 
-/**
- * @description Get the user's data from the contact form
- * @returns An object containing the user's data
- */
+// returns human data values provided in form based on metric
+//or imperial choices
 function getHumanData() {
   let height, weight, units;
 
@@ -212,14 +159,8 @@ function getHumanData() {
   return humanData;
 }
 
-/**
- * @description Creates a grid element for the human object
- * @param {Object} humanData Data grabbed from the user's input form
- * @returns {Element} An element to be added to the grid in the UI
- */
-// REFACTOR TO SPLICE THIS DIRECTLY INTO DinoArray
+// REFACTOR TO SPLICE THIS DIRECTLY INTO dinoArray??
 function createHumanElement(humanData) {
-  // Create the human element for the grid, with user's name and an image
   const newDiv = document.createElement("div");
   newDiv.className = "grid-item";
   newDiv.innerHTML = `<h3>${humanData.name}</h3><img src="images/human.png" alt="image of human">`;
@@ -227,21 +168,15 @@ function createHumanElement(humanData) {
   return newDiv;
 }
 
-/**
- * @description Resets the UI elements if the user wants to try again
- */
-// function repeat() {
-//   document.getElementById("error").innerHTML = "";
-//   document.getElementById("grid").innerHTML = "";
-//   document.getElementById("repeat-btn").style.display = "none";
-//   document.querySelector("form").style.display = "block";
-// }
+// resets the UI to try again
+function repeat() {
+  document.getElementById("error").innerHTML = "";
+  document.getElementById("grid").innerHTML = "";
+  document.getElementById("repeat-btn").style.display = "none";
+  document.querySelector("form").style.display = "block";
+}
 
-/**
- * @description Creates the grid for the UI result
- * @param {Array} dinoArray Array of dinosaur objects
- * @param {Object} humanData The user's data object
- */
+// takes in dinoArray and humanData to create the output
 function updateUI(dinoArray, humanData) {
   document.querySelector("form").style.display = "none";
 
@@ -258,29 +193,31 @@ function updateUI(dinoArray, humanData) {
 
     fragment.appendChild(gridSquare);
   }
-  // Attach fragment with grid elements to the DOM
+  // Add tiles to DOM
+  // attach fragment with grid elements to the DOM
   document.getElementById("grid").appendChild(fragment);
-  // Show the 'Go Again' button
+  // show the 'Go Again' button
   document.getElementById("repeat-btn").style.display = "block";
 }
 
-/**
- * @description Called when the user clicks the submit button, main function of the program which calls the other parts of the sequence
- * @param {event} e The click event on the form's submit button
- */
+// Main function that runs on event
 function clicked(e) {
   // Prevent default page reloading on submit
+  // https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
   e.preventDefault();
 
   const humanData = getHumanData();
 
   const errorMessage = document.getElementById("error");
+  // ensures value is not empty
   if (humanData.name === "") {
     errorMessage.innerHTML = "<p>Please enter a name</p>";
     return;
+    // ensures subject enters an earthly weight
   } else if (humanData.height < 1) {
     errorMessage.innerHTML = "<p>Please enter a height more than 0</p>";
     return;
+    // ensures subject has a measurement
   } else if (humanData.weight < 1) {
     errorMessage.innerHTML = "<p>Please enter a weight more than 0</p>";
     return;
@@ -291,22 +228,10 @@ function clicked(e) {
   updateUI(dinoArray, humanData);
 }
 
-/**
- * @description Called when the user changes units in the form, sets or hides the metric and imperial height and weight input elements
- */
-function chMeasureUnits() {
-  if (document.getElementById("metric").checked) {
-    document.getElementById("metric-form").style.display = "block";
-    document.getElementById("imperial-form").style.display = "none";
-  } else {
-    document.getElementById("metric-form").style.display = "none";
-    document.getElementById("imperial-form").style.display = "block";
-  }
-}
-
-/**
- * @description IIFE to attach the event listeners on the buttons
- */
+// Use IIFE to get human data from form -> this IIFE satisfies this as
+// it's invoked in clicked(). I could have placed getHumanData() in
+// clicked() as IIFE but that would too monolithic.
+// On button click, prepare and display infographic
 (function () {
   document.getElementById("btn").addEventListener("click", clicked);
   document.getElementById("repeat-btn").addEventListener("click", repeat);
