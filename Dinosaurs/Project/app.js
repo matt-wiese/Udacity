ad7c4b94cc9a600e9418fb8d68586d8816066dc8
Matt Wiese <c-matt.wiese@sana.com>
2020-12-16 16:00:00 -0800

    nothing really but breaking out functions

--- Dinosaurs/Project/app.js
+++ Dinosaurs/Project/app.js
@@ -1,5 +1,6 @@
 const button = document.getElementById("btn");
-
+const humanVals = document.getElementById("dino-compare");
+const gridContentContainer = document.getElementById("grid");
 // creating a class to separate out valueas to be compared -> humans
 // share those dimension, although measurements differ greatly lol
 // this will be extended
@@ -22,6 +23,90 @@ class Dinos extends Being {
     this.when = dinoObj.when;
     this.fact = dinoObj.fact;
   }
+  compareWeight(humanWeight) {
+    const ratioW = (this.weight / humanWeight).toFixed(1);
+    // Check for human less than, greater than, or same weight as dino
+    if (ratioW > 1) {
+      return `${this.species} weighed ${(this.weight / humanWeight).toFixed(
+        1
+      )} times more than you!`;
+    }
+    if (ratioW < 1) {
+      return `You weigh ${(humanWeight / this.weight).toFixed(
+        1
+      )} times more than ${this.species}!`;
+    }
+    return `You weigh the same as ${this.species}!`;
+  }
+  compareHeight(humanHeight) {
+    const heightRatio = (this.height / humanHeight).toFixed(1);
+    // Check for human less than, greater than, or same weight as dino
+    if (heightRatio > 1) {
+      return `${this.species} was ${(this.height / humanHeight).toFixed(
+        1
+      )} times taller than you!`;
+    }
+    if (heightRatio < 1) {
+      return `You are ${(humanHeight / this.height).toFixed(
+        1
+      )} times taller than ${this.species}!`;
+    }
+    return `You are the same height as ${this.species}!`;
+  }
+  compareDiet(humanDiet) {
+    //'An' omnivore or 'a' herbivore/carnivore
+    const article = humanDiet === "omnivore" ? "an" : "a";
+
+    if (humanDiet === this.diet) {
+      return `You are ${article} ${humanDiet} and ${this.species} was too!`;
+    } else {
+      return `You are ${article} ${humanDiet}, but ${this.species} was a ${this.diet}.`;
+    }
+  }
+}
+
+class Human extends Being {
+  constructor(humanObj) {
+    super();
+    this.name;
+  }
+}
+
+getRandomFact() {
+  // Returns an integer random number between min (included) and max (included):
+  // reference: https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
+  let randomNumber = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+  //TODO: return random fact here
+  let fact = "";
+  if (this.getSpecies() === "Pigeon") {
+      return this.getFact();
+  }
+  switch (randomNumber) {
+      case 1:
+          fact = this.getWeightComparisonResult();
+          break;
+      case 2:
+          fact = this.getHeightComparisonResult();
+          break;
+      case 3:
+          fact = this.getDietComparisonResult();
+          break;
+      case 4:
+          fact = this.getOrigin();
+          break;
+      case 5:
+          fact = this.getPeriod();
+          break;
+      case 6:
+          fact = this.getFact();
+          break;
+  }
+  return fact;
+}
+
+getHTMLContent() {
+  return `<div class="grid-item"> <h3>${this.species}</h3> <img src="./images/${this.species.toLowerCase()}.png" alt="dino image"> <p>${this.getRandomFact()}</p> </div>`;
+}
 }
 
 // let compHeight = (function (humHgt) {
@@ -37,8 +122,8 @@ class Dinos extends Being {
 // Use IIFE to get human data from form
 let HumanData = (function () {
   let name = document.getElementById("name");
-  let heightInFeet = document.getElementById("feet");
-  let heightInInches = document.getElementById("inches");
+  let feet = document.getElementById("feet");
+  let inches = document.getElementById("inches");
   let weight = document.getElementById("weight");
   let diet = document.getElementById("diet");
 
@@ -51,17 +136,17 @@ let HumanData = (function () {
 
   function getHeight() {
     if (
-      !heightInFeet ||
-      !heightInInches ||
-      !heightInFeet.value ||
-      !heightInInches.value ||
-      heightInFeet.value === "" ||
-      !heightInInches.value === ""
+      !feet ||
+      !inches ||
+      !feet.value ||
+      !inches.value ||
+      !feet.value === "" ||
+      !inches.value === ""
     ) {
       throw new Error("Pelase enter your height");
     }
     // converts to total inches
-    return heightInFeet.value * 12 + heightInInches.value;
+    return feet.value * 12 + inches.value;
   }
 
   function getWeight() {
@@ -71,13 +156,18 @@ let HumanData = (function () {
     return weight.value;
   }
 
+  function getDiet() {
+    return diet.value;
+  }
+
   return {
     getName: getName,
     getHeight: getHeight,
     getWeight: getWeight,
-    getDiet: diet.value,
+    getDiet: getDiet,
   };
 })();
+
 // Create Dino Compare Method 1
 // NOTE: Weight in JSON file is in lbs, height in inches.
 
@@ -92,3 +182,12 @@ let HumanData = (function () {
 // Add tiles to DOM
 
 // Remove form from screen
+
+button.addEventListener("click", () => {
+  document.querySelector("form").style.display = "none";
+  readDinosJSON();
+
+  // get human data from fields and include error messages
+  // create the dino elements from array
+  //
+});
